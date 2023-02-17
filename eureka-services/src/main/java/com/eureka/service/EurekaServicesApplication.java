d28fd86e5d2a65ccf3e786fd385e4f25ff095f93
codejojov <joanvrivas.21@gmail.com>
2023-02-17 01:25:38 +0100

    eureka

---
+++ eureka-services/src/main/java/com/eureka/service/EurekaServicesApplication.java
@@ -0,0 +1,15 @@
+package com.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer 
+public class EurekaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServicesApplication.class, args);
+	}
+
+}
