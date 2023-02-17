50a31fef1567aa9c7904759177d54a1713b08e02
codejojov <joanvrivas.21@gmail.com>
2023-02-17 17:24:20 +0100

    gateway

---
+++ gateway-services/src/test/java/com/gateway/service/GatewayServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
